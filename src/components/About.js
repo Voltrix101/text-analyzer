0f4b9f60334a37f04f6a7e82c3542a9a84e8e95d
Voltrix101 <amritob0327.roy@gmail.com>
2024-03-12 20:52:05 +0530

    made some title changes

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ export default function About(props) {
           </h2>
           <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-              Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or
+              TextAnalyzer gives you a way to analyze your text quickly and efficiently. Be it word count, character count or
             </div>
           </div>
         </div>
@@ -34,7 +34,7 @@ export default function About(props) {
           </h2>
           <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
             <div className="accordion-body" style={myStyle}>
-              TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+              TextAnalyzer is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word character limit.
             </div>
           </div>
         </div>
